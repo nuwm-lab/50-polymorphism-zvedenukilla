8139c44926c9e036c966c52919ce82d31c71e9c1
Ілля <zvedeniuk_ak24@nuwm.edu.ua>
2025-11-05 14:45:20 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,90 @@
-﻿using System;
+using System;
 
-namespace LabWork
+namespace LinearAndPolynomial
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    
+    class LinearFunction
     {
-        static void Main(string[] args)
+        protected double a1;
+        protected double a0;
+
+        
+        public virtual void SetCoefficients()
         {
+            Console.Write("Введіть коефіцієнт a1: ");
+            a1 = double.Parse(Console.ReadLine());
+            Console.Write("Введіть коефіцієнт a0: ");
+            a0 = double.Parse(Console.ReadLine());
+        }
+
+        public virtual void DisplayCoefficients()
+        {
+            Console.WriteLine($"Лінійна функція: f(x) = {a1}x + {a0}");
+        }
+
+       
+        public virtual double Calculate(double x)
+        {
+            return a1 * x + a0;
+        }
+    }
+
+    
+    class Polynomial : LinearFunction
+    {
+        protected double a2, a3, a4;
+
+        
+        public override void SetCoefficients()
+        {
+            Console.Write("Введіть коефіцієнт a4: ");
+            a4 = double.Parse(Console.ReadLine());
+            Console.Write("Введіть коефіцієнт a3: ");
+            a3 = double.Parse(Console.ReadLine());
+            Console.Write("Введіть коефіцієнт a2: ");
+            a2 = double.Parse(Console.ReadLine());
+            Console.Write("Введіть коефіцієнт a1: ");
+            a1 = double.Parse(Console.ReadLine());
+            Console.Write("Введіть коефіцієнт a0: ");
+            a0 = double.Parse(Console.ReadLine());
+        }
+
+        public override void DisplayCoefficients()
+        {
+            Console.WriteLine($"Многочлен: f(x) = {a4}x^4 + {a3}x^3 + {a2}x^2 + {a1}x + {a0}");
+        }
+
+
+        public override double Calculate(double x)
+        {
+            return a4 * Math.Pow(x, 4) + a3 * Math.Pow(x, 3) + a2 * Math.Pow(x, 2) + a1 * x + a0;
+        }
+    }
+
+    internal static class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+           
+            LinearFunction linear = new LinearFunction();
+            Console.WriteLine("=== ЛІНІЙНА ФУНКЦІЯ ===");
+            linear.SetCoefficients();
+            linear.DisplayCoefficients();
+
+           
+            Polynomial poly = new Polynomial();
+            Console.WriteLine("\n=== МНОГОЧЛЕН ===");
+            poly.SetCoefficients();
+            poly.DisplayCoefficients();
+
+            Console.Write("\nВведіть значення x: ");
+            double x = double.Parse(Console.ReadLine());
+
             
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"\nЗначення лінійної функції в точці x={x}: {linear.Calculate(x)}");
+            Console.WriteLine($"Значення многочлена в точці x={x}: {poly.Calculate(x)}");
         }
     }
 }
